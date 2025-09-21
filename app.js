f8be06573cb7634ad33bd70137b401553e445235
Chhayakanta Dash <chhayakantdash143@gmail.com>
2025-09-21 12:43:03 +0530

    signup

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wonderlust";
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js"); 
 const ExpressError = require("./utils/ExpressError");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js"); 
+const userRouter = require("./routes/user.js");
 
 
 
@@ -51,6 +55,11 @@ app.get("/",(req,res) =>{
 
 app.use(session(sessionOptions));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 app.use((req,res,next) =>{
@@ -59,9 +68,19 @@ app.use((req,res,next) =>{
     next();
 });
 
+// app.get("/demouser", async (req,res) =>{
+//     let fakeUser = new User({
+//         username: "manish",
+//         email: "lV5tK@example.com",
+//     });
+//     let registerUser = await User.register(fakeUser,"hello123");
+//     res.send(registerUser);
+// });
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews",reviews);
+
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews",reviewRouter);
+app.use("/",userRouter);
 
 app.use ((err,req,res,next) =>{
      let { statusCode = 500, message = "Oh No, Something Went Wrong!" } = err;
